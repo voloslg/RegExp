69c44aac715f4fab15870010fd9279a4030de99c
volosgoto <volosgoto@yahoo.com>
2019-03-11 08:54:00 +0200

    remove metacharacters.js

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
 let re;
 // Literal characters
 re = /hello/;
-re = /hello/i; // case  
+re = /hello/i; // case
 
-
-// Metacharacters Symhols 
-re = /^h/i;           // must starts with   
-re = /d$/i;           // must ends with   
-re = /world$/i;       // must ends with    
-re = /^hello$/i;      // must begins adn ends with    
-re = /h.llo$/i;       // matches any only ONE character    
-re = /h*llo$/i;       // matches any character 0 or more times    
-re = /gra?e?y/i;      // optional character (a is optional, e is optional)    
-re = /gra?e?y\?/i;    // escape character ('gray?')
-re = /gre*n/;
-
-
-
-
+// Metacharacters Symhols
+re = /^h/i; // must starts with
+re = /d$/i; // must ends with
+re = /world$/i; // must ends with
+re = /^hello$/i; // must begins adn ends with
+re = /h.llo$/i; // matches any only ONE character
+re = /h*llo$/i; // matches any character 0 or more times
+re = /gra?e?y/i; // optional character (a is optional, e is optional)
+re = /gra?e?y\?/i; // escape character ('gray?')
 
 // Str to match
 // let str = 'Hello World';
-let str = 'greeen';
+let str = "greeen";
 console.log(re.source);
 let result = re.exec(str);
-console.log('from re.exec()', result);
+console.log("from re.exec()", result);
 
 function reTest(re, str) {
   if (re.test(str)) {
     console.log(`${str} matched ${re.source}`);
   } else {
-    console.log('from reTest(re, str)', `${str} does NOT match ${re.source}`);
+    console.log("from reTest(re, str)", `${str} does NOT match ${re.source}`);
   }
 }
 reTest(re, str);
