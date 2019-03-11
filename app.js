ff611db86f08d92d95a03b61fe05105598eeeb50
volosgoto <volosgoto@yahoo.com>
2019-03-11 11:32:44 +0200

    all basics

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let re;
 // Literal characters
 re = /hello/;
-re = /hello/i; // case
+re = /hello/i; // case insencitive
+re = /hello/g; // global, search all matches
 
 // Metacharacters Symhols
 re = /^h/i; // must starts with
+re = /^hello/i; // must starts with
 re = /d$/i; // must ends with
 re = /world$/i; // must ends with
 re = /^hello$/i; // must begins adn ends with
@@ -13,18 +15,54 @@ re = /h*llo$/i; // matches any character 0 or more times
 re = /gra?e?y/i; // optional character (a is optional, e is optional)
 re = /gra?e?y\?/i; // escape character ('gray?')
 
-// Str to match
+// Breckets [] - character sets
+re = /gr[ae]y/i; // Must be inside [a or e]
+re = /[GF]ray/i; // Must be inside [G or F]
+re = /[^GF]ray/i; // Match anythig exept [G or F]
+re = /^[GF]ray/i; // Must be begin with [G or F]
+re = /[A-Z]ray/; // Begin's with any uppercase letter
+re = /[a-z]ray/; // Begin's with any lowercase letter
+re = /[A-Za-z]ray/; // Any letter (case insencitive)
+re = /[0-9]ray/; // Any digit number
+re = /[0-4]ray/; // Any digit from 0 to 4
+
+// Braces {} Quantifiers
+re = /Hel{2}o/i; // Match exacly {n} amount times.
+re = /Hel{2,4}o/i; // Match range {form n, to m}.
+re = /Hel{2,}o/i; // At least {n timres}.
+
+// Parentheses () Grouping
+re = /^([0-9]x){3}$/i;
+
+// Shorthand character classes
+re = /\w/; // Word character. Only letter, number, or _ (undrscore).
+re = /\w+/; // One ore more word character.  + = one ore more
+re = /\W/; // NON word character. () [] {} @
+re = /\d/; // Match any digit number
+re = /\d+/; // Match any digit number one or more times
+re = /\D/; // NON digit number
+re = /\s/; // Match whitespace charracter
+re = /\S/; // NON whitespace charracter
+re = /Hell\b/; // Word boundary. Only  Hell word, Hello is false
+
+// Assertions
+re = /x(?=y)/; // Match only if its before y
+re = /x(?!y)/; // Match only if NOT before y
+
+// String to match
 // let str = 'Hello World';
-let str = "greeen";
-console.log(re.source);
+let str = "xy";
 let result = re.exec(str);
 console.log("from re.exec()", result);
 
 function reTest(re, str) {
   if (re.test(str)) {
-    console.log(`${str} matched ${re.source}`);
+    console.log(`YES ${str} matched ${re.source}`);
   } else {
-    console.log("from reTest(re, str)", `${str} does NOT match ${re.source}`);
+    console.log(
+      "NO from reTest(re, str)",
+      `${str} does NOT match ${re.source}`
+    );
   }
 }
 reTest(re, str);
